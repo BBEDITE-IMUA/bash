f41517f5bf0c6473f5ccbfb27079e15564c8d7f8
Ваше Имя <you@example.com>
2023-05-07 21:43:30 +0300

    upload

---
+++ 1_test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+tempDir=tmp
+SourceDir=source
+remove(){
+rm -r $tempDir
+rm -r $SourceDir
+}
+mkdir $tempDir
+mkdir $SourceDir
+echo "Test Text" > $SourceDir/testFile.txt
+
+bash $1 "$SourceDir" "$tempDir"
+
+if [[ ! -f "$tempDir/$SourceDir/testFile.txt" ]]
+then
+    echo "файл не был скопирован"
+    remove
+    exit 1
+fi
+
+hash1=$(sha256sum $SourceDir/testFile.txt | awk '{ print $1 }')
+
+if [ "$hash1" == "$hash2" ]
+then
+    echo "тест пройден успешно"
+    remove
+    exit 0
+else
+    echo "файл скопирован неправильно"
+    remove
+    exit 2
+fi
